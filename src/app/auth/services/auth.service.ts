db6621b1b8264728b583442f054c710b4fd259aa
ALEJANDRO3410A <maa.ortiz@yavirac.edu.ec>
2024-12-16 21:43:32 -0500

    Recuperar contraseña

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,34 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private baseUrl="http://127.0.0.1:3000" //solo para pruebas deberia ir "http://127.0.0.1:3000"
+  private baseUrl="http://127.0.0.1:3000";  // Solo para pruebas
 
-  private http=inject(HttpClient)
+  private http=inject(HttpClient);
 
   constructor() { }
 
-  loginConNest(credenciales: any){
+  loginConNest(credenciales: any) {
     return this.http.post<any>(`${this.baseUrl}/auth/login`, credenciales); 
   }
 
-  registroConNest(datos: any){
-    return this.http.post<any>(`${this.baseUrl}/register`,datos);
+  registroConNest(datos: any) {
+    return this.http.post<any>(`${this.baseUrl}/auth/register`, datos); 
   }
 
   forgotPassword(email: string): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/auth/forgot-password`, { email });
   }
 
-  // Método para restablecer la contraseña
   resetPassword(token: string, newPassword: string): Observable<any> {
-    // Aquí pasamos el token como parámetro en la URL y la nueva contraseña en el cuerpo
     return this.http.post<any>(`${this.baseUrl}/auth/reset-password?token=${token}`, { newPassword });
   }
-  
 }
